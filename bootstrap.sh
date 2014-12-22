c1409d7082a37ce492574b6c48d9b96b260bf8ab
Seb Jacobs <me@sebjacobs.com>
2014-12-22 22:53:09 +0000

    Make use of the homebrew services plugin

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,8 @@ brew install pow
 brew install rbenv ruby-build
 brew install redis
 
+brew tap homebrew/boneyard
+
 brew install caskroom/cask/brew-cask
 brew cask install alfred
 brew cask install android-studio

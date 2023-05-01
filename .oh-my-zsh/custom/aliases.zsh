8fcef05687deccd296225e0c2f81c4bbd0bb7457
Seb Jacobs <me@sebjacobs.com>
2023-05-01 20:50:32 +0100

    add convert_ac3_to_aac script

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -7,3 +7,14 @@ alias pyman="poetry run python manage.py"
 
 alias intel="env /usr/bin/arch -x86_64 /bin/zsh"
 alias arm="env /usr/bin/arch -arm64 /bin/zsh"
+
+function convert_ac3_to_aac() {
+  filename=$1
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  output_filename="$filename-aac.$extension"
+
+  ffmpeg -i $1 -vcodec copy -scodec copy -acodec libfdk_aac -b:a 640k -ac 6 -map 0 $output_filename
+}
+
+alias ac3_to_aac="convert_ac3_to_aac"

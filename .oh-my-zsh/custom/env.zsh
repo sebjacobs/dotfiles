c594b17a3c274be8a8a335a52438b3b946db443a
Seb Jacobs <me@sebjacobs.com>
2023-07-28 12:04:06 +0100

    Use "$HOME" for home prefix
    Rather than hardcoded paths, or ~ shorthand

--- .oh-my-zsh/custom/env.zsh
+++ .oh-my-zsh/custom/env.zsh
@@ -5,7 +5,7 @@ export JAVA_HOME=$(/usr/libexec/java_home -v 20)
 #export PATH="$HOME/.jenv/bin:$PATH"
 #eval "$(jenv init -)"
 
-export ANDROID_HOME=~/Library/Android/sdk
+export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
@@ -20,7 +20,7 @@ export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
 
 #export RUBY_CONFIGURE_OPTS="--with-zlib-dir=$(brew --prefix zlib) --with-openssl-dir=$(brew --prefix openssl@1.1) --with-readline-dir=$(brew --prefix readline) --with-libyaml-dir=$(brew --prefix libyaml)"
 
-export DENO_INSTALL="/Users/sebjacobs/.deno"
+export DENO_INSTALL="$HOME/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
 export PYENV_ROOT="$HOME/.pyenv"
@@ -29,7 +29,7 @@ eval "$(pyenv init -)"
 
 # python poetry setup
 export PIP_REQUIRE_VIRTUAL_ENV=true
-export PATH="/Users/sebjacobs/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 export PATH="/opt/homebrew/opt/dotnet@6/bin:$PATH"
 export DOTNET_ROOT="/opt/homebrew/opt/dotnet@6/libexec"

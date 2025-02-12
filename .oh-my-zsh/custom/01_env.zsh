0146abb9a2795feab3b3b5cca006896aa2fa4805
Seb Jacobs <me@sebjacobs.com>
2025-02-12 11:56:22 +0000

    Tweak asdf setup

--- .oh-my-zsh/custom/01_env.zsh
+++ .oh-my-zsh/custom/01_env.zsh
@@ -37,8 +37,8 @@ export PATH="$PATH":"$HOME/.pub-cache/bin"
 
 export EDITOR='code --wait'
 
-## Ruby version management
-. $BREW_PREFIX/opt/asdf/libexec/asdf.sh
+## Ruby version management (ASDF)
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
 ## NodeJS version management
 export VOLTA_HOME="$HOME/.volta"

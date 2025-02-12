2582c80821a91828b44c4f0a0d790113fa7176ca
Seb Jacobs <me@sebjacobs.com>
2025-02-12 11:55:05 +0000

    Remove support for Intel-based macs

--- .oh-my-zsh/custom/00_brew.zsh
+++ .oh-my-zsh/custom/00_brew.zsh
@@ -1,8 +1,3 @@
-if test -d "/opt/homebrew"; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-  export BREW_PREFIX=/opt/homebrew
-else
-  eval "$(/usr/local/bin/brew shellenv)"
-  export BREW_PREFIX=/usr/local
-fi
+eval "$(/opt/homebrew/bin/brew shellenv)"
+export BREW_PREFIX=/opt/homebrew
 
